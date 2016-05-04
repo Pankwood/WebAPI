06cc5347a9320b324802cbb765646e440b15cb5b
Danilo Debiazi <danilo.debiazi@yahoo.com.br>
2016-05-04 15:33:00 -0300

    Altered method with HttpResponseMessage

--- ProductsApp/Controllers/ProductController.cs
+++ ProductsApp/Controllers/ProductController.cs
@@ -9,7 +9,12 @@
 #region Referencies
 using Company.Business.Context;
 using Company.DomainModel;
+using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Web.Http;
 #endregion
 
@@ -18,39 +23,149 @@ namespace ProductsApp.Controllers
     [RoutePrefix("product")]
     public class ProductController : ApiController
     {
-        public IEnumerable<Product> Get()
+        public HttpResponseMessage Get()
         {
+            HttpResponseMessage response;
             ProductBUS product = new ProductBUS();
+            
+            try
+            {
+                IEnumerable<Product> products = product.Get();
 
-            return product.Get();
+                if (products == null)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.NotFound, product);
+                    return response;
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK, products);
+                    response.Headers.CacheControl = new CacheControlHeaderValue()
+                    {
+                        MaxAge = TimeSpan.FromMinutes(20)
+                    };
+
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+                return response;
+
+            }
         }
 
-        public Product Get(int id)
+        public HttpResponseMessage Get(int id)
         {
+            HttpResponseMessage response;
             ProductBUS product = new ProductBUS();
 
-            return product.GetByID(id);
+            try
+            {
+                Product products = product.GetByID(id);
+
+                if (products == null)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.NotFound, products);
+                    return response;
+                }
+                else 
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK, products);
+                    response.Headers.CacheControl = new CacheControlHeaderValue()
+                    {
+                        MaxAge = TimeSpan.FromMinutes(20)
+                    };
+
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+                return response;
+            }
         }
 
-        public IEnumerable<Product> Post([FromBody]Product pProduct)
+        public HttpResponseMessage Post([FromBody]Product pProduct)
         {
+            HttpResponseMessage response;
             ProductBUS product = new ProductBUS();
 
-            return product.Post(pProduct);
+            try
+            {
+                IEnumerable<Product> products = product.Post(pProduct);
+                if (products == null)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.NotFound, products);
+                    return response;
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK, products);
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+                return response;
+            }
         }
 
-        public IEnumerable<Product> Put(int id, [FromBody]Product pProduct)
+        public HttpResponseMessage Put(int id, [FromBody]Product pProduct)
         {
+            HttpResponseMessage response;
             ProductBUS product = new ProductBUS();
 
-            return product.Put(id, pProduct);
+            try
+            {
+                IEnumerable<Product> products = product.Put(id, pProduct);
+
+                if (products == null)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.NotFound, products);
+                    return response;
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK, products);
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+                return response;
+            }
         }
 
-        public IEnumerable<Product> Delete(int id)
+        public HttpResponseMessage Delete(int id)
         {
+            HttpResponseMessage response;
             ProductBUS product = new ProductBUS();
 
-            return product.Delete(id);
+            try
+            {
+                IEnumerable<Product> products = product.Delete(id);
+                if (products == null)
+                {
+                    response = Request.CreateResponse(HttpStatusCode.NotFound, products);
+                    return response;
+                }
+                else
+                {
+                    response = Request.CreateResponse(HttpStatusCode.OK, products);
+                    return response;
+                }
+            }
+            catch (Exception e)
+            {
+                response = Request.CreateResponse(HttpStatusCode.InternalServerError, e.Message);
+                return response;
+            }
+            //return product.Delete(id);
         }
     }
 }
